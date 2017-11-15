edde435a7cbe4020fb40879f10d335dfa07b5fec
Wilson Leong <wilson.leong.0@gmail.com>
2017-11-15 12:11:30 -0800

    got the structs working test cases were a bust. Working on interp, got the first case for NumV working

--- assign3_enums_version_attempt.rs
+++ assign3_enums_version_attempt.rs
@@ -1,5 +1,14 @@
 //Assignment 3
 
+//assert_eq!(4, add_two(2)); // expected result, func call
+
+/*fn test_interp() { 
+	
+	//assert_eq!(4, 2+2);
+	//assert_eq!(6, interp(ExprC::numC(4)) );
+	//assert_eq!(ExprC::numC{val: 4}, ExprC::numC{val: 4});
+}*/	
+
 enum ExprC { 
 
 	numC { 
@@ -18,35 +27,99 @@ enum ExprC {
 		val: bool
 	},	
 
-	ifC { 
-		test: <Box<ExprC>>,
-		then: <Box<ExprC>>,
-		last: <Box<ExprC>>
-	},	
-
 	lamC { 
 		params: Vec<char>,
-		body: <Box<ExprC>>
+		body: Box<ExprC>
+	},	
 
+	ifC { 
+		test: Box<ExprC>,
+		then: Box<ExprC>,
+		last: Box<ExprC>
 	},	
 
 	binopC { 
 		
 		op: char,
-		left : <Box<ExprC>>,
-		right : <Box<ExprC>>
+		left : Box<ExprC>,
+		right : Box<ExprC>
 	},	
 
 	appC { 
 
-		func : <Box<ExprC>>,
-		args : Vec< <Box<ExprC>> >
+		func : Box<ExprC>,
+		args : Vec<Box<ExprC>>
+	}
+
+}	
+
+struct Binding { 
+	name: char,
+	val: Value
+}
+
+
+struct Env { 
+	bindings: Vec<Binding>
+	
+}	
+
+//return values for interpreter
+enum Value { 
+
+	numV { 
+		n: i64
+
+	},
+
+	boolV {
+		b: bool
+	},	
+
+	closV { 
+
+		args: Vec<char>,
+		body: Box<ExprC>,
+		env: Env
 	}
 
 }	
 
 fn main() { 
 	
-
+	//test_interp();	
 
 }	
+
+fn interp(expr: ExprC, env: Env) -> Value {
+
+	match expr {
+		
+		ExprC::numC{val: v} => Value::numV{n: v}
+
+	/*ExprCidC::expr.char
+        },
+
+	trueC {	
+                val: bool
+        },
+
+	falseC {
+                val: bool
+        },
+
+	lamC {
+              	params: Vec<char>,
+                body: Box<ExprC>
+        },
+
+	ifC {
+             	test: Box<ExprC>,
+                then: Box<ExprC>,
+                last: Box<ExprC>
+        },*/
+
+
+	}
+
+}
